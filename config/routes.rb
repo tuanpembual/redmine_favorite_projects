e16f72593382135425760abf4a5c62ecddbeacf1
Alexander Meindl <a.meindl@alphanodes.com>
2015-08-06 19:42:41 +0200

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 #custom routes for this plugin
-match "favorite_projects/:action" => "favorite_projects"
-match "favorite_projects/search" => "favorite_projects#search", :as => "search_favorite_projects"
+match 'favorite_projects/:action', :to => 'favorite_projects', via: [:get, :put, :post]
+match 'favorite_projects/search', :to => 'favorite_projects#search', :as => 'search_favorite_projects', via: [:get, :put, :post]
